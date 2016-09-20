fe5437206f3cabf8be06b413df9528064b09a833
marcoputon <marcoputon@gmail.com>
2016-09-20 14:59:50 -0300

    adição de campos no cadastro e no banco

--- database building/inak.sql
+++ database building/inak.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.0.10deb1
 -- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: 30-Ago-2016 às 20:29
--- Versão do servidor: 10.1.13-MariaDB
--- PHP Version: 7.0.5
+-- Servidor: localhost
+-- Tempo de Geração: 20/09/2016 às 14:57
+-- Versão do servidor: 5.5.50-0ubuntu0.14.04.1
+-- Versão do PHP: 5.5.9-1ubuntu4.19
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,103 +14,80 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
--- Database: `inak`
+-- Banco de dados: `inak`
 --
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `card`
+-- Estrutura para tabela `card`
 --
 
-CREATE TABLE `card` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `card` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `front` varchar(200) NOT NULL,
   `back` varchar(200) NOT NULL,
   `lastPlay` datetime NOT NULL,
   `nextPlay` datetime NOT NULL,
-  `deck` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `deck` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `deck` (`deck`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `deck`
+-- Estrutura para tabela `deck`
 --
 
-CREATE TABLE `deck` (
-  `id` int(11) NOT NULL,
+CREATE TABLE IF NOT EXISTS `deck` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(30) NOT NULL,
   `shared` tinyint(1) NOT NULL,
   `nextCardPlay` datetime DEFAULT NULL,
-  `ownerId` varchar(90) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `ownerId` varchar(90) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ownerId` (`ownerId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `user`
+-- Estrutura para tabela `user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE IF NOT EXISTS `user` (
   `email` varchar(90) NOT NULL,
   `password` varchar(256) NOT NULL,
-  `picture` varchar(36) NOT NULL
+  `picture` varchar(36) NOT NULL,
+  `userName` varchar(40) NOT NULL,
+  PRIMARY KEY (`userName`),
+  UNIQUE KEY `email` (`email`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Indexes for dumped tables
+-- Fazendo dump de dados para tabela `user`
+--
+
+INSERT INTO `user` (`email`, `password`, `picture`, `userName`) VALUES
+('oir@oir', '04f5c98ab110048e3a98e3637ca47a35a04c76c9', '', '04f5c98ab110048e3a98e3637ca47a35a04c76c9'),
+('oi@oi', 'ef67e0868c98e5f0b0e2fcd9b0c4a3bad808f551', '', 'ef67e0868c98e5f0b0e2fcd9b0c4a3bad808f551');
+
+--
+-- Restrições para dumps de tabelas
 --
 
 --
--- Indexes for table `card`
---
-ALTER TABLE `card`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `deck` (`deck`);
-
---
--- Indexes for table `deck`
---
-ALTER TABLE `deck`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `ownerId` (`ownerId`);
-
---
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`email`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `card`
---
-ALTER TABLE `card`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT for table `deck`
---
-ALTER TABLE `deck`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
---
--- Constraints for dumped tables
---
-
---
--- Limitadores para a tabela `card`
+-- Restrições para tabelas `card`
 --
 ALTER TABLE `card`
   ADD CONSTRAINT `card_ibfk_1` FOREIGN KEY (`deck`) REFERENCES `deck` (`id`) ON DELETE CASCADE;
 
 --
--- Limitadores para a tabela `deck`
+-- Restrições para tabelas `deck`
 --
 ALTER TABLE `deck`
   ADD CONSTRAINT `deck_ibfk_1` FOREIGN KEY (`ownerId`) REFERENCES `user` (`email`) ON DELETE CASCADE ON UPDATE CASCADE;
