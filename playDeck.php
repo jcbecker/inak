8520ba12354fb144351170854efa2d139a5f825c
Marco Aurélio <Marco Aurlio>
2016-12-18 00:58:16 -0200

    Esquema pra mostrar as cartas que devem ser exibidas na hora certa

--- playDeck.php
+++ playDeck.php
@@ -4,6 +4,10 @@
 	$query = "select * from deck where id_deck = '".$_GET['id_deck']."'";
 	$res =  mysqli_query($con, $query);
 	$aux = $res->fetch_assoc();
+
+	$query = "select * from card where id_deck = ".$_GET['id_deck']." and card.nextPlay <= CURTIME();";
+	$res =  mysqli_query($con, $query);
+	$card = $res->fetch_assoc();	// Card from the top of the cards to play
 ?>
 <div class="container" style="margin-bottom:50px;">
 	<h2><span class="label label-primary"><?php echo $aux['name'] ?> </span></h2>
@@ -11,7 +15,7 @@
 		<div class="col-md-6" id ="profile_body" style="margin-top:50px;">
 			<div class="panel panel-default">
   					<div class="panel-body">
-   					 Conteúdo
+   					 <?php echo $card["front"]; ?>
   					</div>
 			</div>
 			<div class="btn-group btn-group-justified" role="group" aria-label="...">
@@ -19,7 +23,7 @@
     				<button type="button" class="btn btn-default">Easy</button>
   				</div>
   			<div class="btn-group" role="group">
-    				<button type="button" class="btn btn-default">Middle</button>
+    				<button type="button" class="btn btn-default">Ok</button>
   				</div>
   			<div class="btn-group" role="group">
     			<button type="button" class="btn btn-default">Hard</button>
